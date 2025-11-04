f7a257672f88c99853910cf5513309f65f813496
JJEverardo <everardosilvajunior@gmail.com>
2025-11-04 14:14:39 -0300

    Add files via upload

---
+++ salas.js
@@ -0,0 +1,19 @@
+function f1(){
+    location.href="blueroom.html"
+}
+
+function f2(){
+    location.href="bedroom.html"
+}
+
+function f3(){
+    location.href="redroom.html"
+}
+
+function f4(){
+    location.href="greenroom.html"
+}
+
+function f5(){
+    location.href="shoproom.html"
+}
